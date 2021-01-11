88072c206790e05d0509c7081f40ba2c646c8ab2
vlad054 <41372107+vlad054@users.noreply.github.com>
2021-01-11 09:58:12 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+int main()
+{
+    int  s = 5;
+    bool t = 1;
+    
+    vector<string> nums = {"one", "onee", "three"};
+    
+    if (nums[0] < nums[1]){
+        cout<<" not equq ";
+    }
+    
+    cout<<"Hello World";
+    cout<<s;
+    cout<<t;
+    cout<<" ";
+    cout<<nums.size();
+    
+    return 0;
+}
